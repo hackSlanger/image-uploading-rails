72d13c7a2ca52d36318f0a40ba87fbf13fd259c1
forriteshbhat <riteshbhat17@gmail.com>
2017-03-23 23:34:42 +0530

    form and model modified

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,10 @@
 class Hotel < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates_processing_of :image
+  validate :image_size_validation
+
+  private
+    def image_size_validation
+      errors[:image] << "The image size should be less than 900KB" if image.size > 0.9.megabytes
+    end
 end
